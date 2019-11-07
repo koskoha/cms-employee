d976bf66867e2417e1598a26a8a6dd4c984d352e
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2019-11-06 22:13:30 -0500

    add: dataloader, create message subscription, test env

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import cors from 'cors';
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import { ApolloServer, AuthenticationError } from 'apollo-server-express';
+import http from 'http';
+import DataLoader from 'dataloader';
 
 import schema from './schema';
 import resolvers from './resolvers';
 import models, { sequelize } from './models';
 import createUsersWithMessages from './seed';
+import loaders from './loaders';
 
 const app = express();
 app.use(cors());
@@ -27,32 +30,47 @@ const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
   formatError: error => {
-    // remove the internal sequelize error message
-    // leave only the important validation error
     const message = error.message.replace('SequelizeValidationError: ', '').replace('Validation error: ', '');
     return {
       ...error,
       message,
     };
   },
-  context: async ({ req }) => {
-    const me = await getMe(req);
-    return {
-      models,
-      me,
-      secret: process.env.SECRET,
-    };
+  context: async ({ req, connection }) => {
+    if (connection) {
+      return {
+        models,
+        loaders: {
+          user: new DataLoader(keys => loaders.user.batchUsers(keys, models)),
+        },
+      };
+    }
+    if (req) {
+      const me = await getMe(req);
+      return {
+        models,
+        me,
+        secret: process.env.SECRET,
+        loaders: {
+          user: new DataLoader(keys => loaders.user.batchUsers(keys, models)),
+        },
+      };
+    }
   },
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-const eraseDatabaseOnSync = true;
-sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
-  if (eraseDatabaseOnSync) {
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+const isTest = !!process.env.TEST_DATABASE;
+
+sequelize.sync({ force: isTest }).then(async () => {
+  if (isTest) {
     createUsersWithMessages(new Date());
   }
-  app.listen({ port: 8000 }, () => {
+  httpServer.listen({ port: process.env.PORT || 8000 }, () => {
     // eslint-disable-next-line no-console
     console.log('Apollo Server on http://localhost:8000/graphql');
   });
--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,18 @@
 import 'dotenv/config';
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-  dialect: 'postgres',
-  port: 54320,
-});
+let sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+  });
+} else {
+  const database = (process.env.TEST_DATABASE && process.env.TEST_DATABASE.trim()) || process.env.DATABASE;
+  sequelize = new Sequelize(database, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+    dialect: 'postgres',
+    port: 5433,
+  });
+}
 const models = {
   User: sequelize.import('./user'),
   Message: sequelize.import('./message'),
